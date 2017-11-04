7e72420194d1dceb0f6be00616de5658761f0016
Rhonda Tinkham <rtinkham@live.com>
2017-11-03 21:08:13 -0400

    added player data

--- SoccerCoordinator/SoccerCoordinator.playground/Contents.swift
+++ SoccerCoordinator/SoccerCoordinator.playground/Contents.swift
@@ -3,3 +3,24 @@
 import UIKit
 
 // Player Data
+
+let players: [[String: Any]] = [
+    ["Name": "Joe Smith", "Height": 42, "Soccer Expeience": true, "Guardian Name(s)": "Jim and Jan Smith"],
+    ["Name": "Jill Tanner", "Height": 36, "Soccer Expeience": true, "Guardian Name(s)": "Clara Tanner"],
+    ["Name": "Bill Bon", "Height": 43, "Soccer Expeience": true, "Guardian Name(s)": "Sara and Jenny Bon"],
+    ["Name": "Eva Gordon", "Height": 45, "Soccer Expeience": false, "Guardian Name(s)": "Wendy and Mike Gordon"],
+    ["Name": "Matt Gill", "Height": 40, "Soccer Expeience": false, "Guardian Name(s)": "Matt Gill"],
+    ["Name": "Kimmy Stein", "Height": 41, "Soccer Expeience": false, "Guardian Name(s)": "Bill and Hillary Stein"],
+    ["Name": "Sammy Adams", "Height": 45, "Soccer Expeience": false, "Guardian Name(s)": "Jeff Adams"],
+    ["Name": "Karl Saygan", "Height": 42, "Soccer Expeience": true, "Guardian Name(s)": "Heather Bledsoe"],
+    ["Name": "Suzane Greenberg", "Height": 44, "Soccer Expeience": true, "Guardian Name(s)": "Henrietta Dumas"],
+    ["Name": "Sal Dali", "Height": 41, "Soccer Expeience": false, "Guardian Name(s)": "Gala Dali"],
+    ["Name": "Joe Kavalier", "Height": 39, "Soccer Expeience": false, "Guardian Name(s)": "Sam and Elaine Kavalier"],
+    ["Name": "Ben Finkelstein", "Height": 44, "Soccer Expeience": false, "Guardian Name(s)": "Aaron and Jill Finkelstein"],
+    ["Name": "Diego Soto", "Height": 41, "Soccer Expeience": true, "Guardian Name(s)": "Robin and Sarika Soto"],
+    ["Name": "Chloe Alaska", "Height": 47, "Soccer Expeience": false, "Guardian Name(s)": "David and Jamie Alaska"],
+    ["Name": "Arnold Willis", "Height": 43, "Soccer Expeience": false, "Guardian Name(s)": "Claire Willis"],
+    ["Name": "Phillip Helm", "Height": 44, "Soccer Expeience": true, "Guardian Name(s)": "Thomas Helm and Eva Jones"],
+    ["Name": "Les Clay", "Height": 42, "Soccer Expeience": true, "Guardian Name(s)": "Wynonna Brown"],
+    ["Name": "Herschel Krustofski", "Height": 45, "Soccer Expeience": true, "Guardian Name(s)": "Hyman and Rachel Krustofski"],
+]
